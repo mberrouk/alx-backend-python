95b6e8ba4298ff800950ac7b3bb083233b0c9a76
mberrouk <mberrouk@student.1337.ma>
2024-10-17 21:22:48 +0100

    init

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,9 +2,9 @@
 """ Basic Async Syntax
 """
 
-
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """ wait and return asyncio.Task
